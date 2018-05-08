46007b448bf9cff8f306cd9d8dfe53aa39b1f06e
zhaolgyu <zhaolgyu@163.com>
2018-05-08 21:04:11 +0800

    111

---
+++ src/main/java/com/lyu/HelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.lyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+
+        // Spring应用启动起来
+        SpringApplication.run(HelloWorldApplication.class,args);
+    }
+}
